14d252e43bc4233ade06fbc12d7f7caafda56760
LizethLux <lizeth.villanueva01@utrgv.edu>
2026-04-22 18:15:38 +0000

    Post model

--- proj1App/models.py
+++ proj1App/models.py
@@ -1,8 +1,9 @@
-from django.db import models
+from django.db import models 
+#importing model system
 
-class Post(models.Model):
-    content = models.TextField()
-    is_public = models.BooleanField(default=True)
+class Post(models.Model):  #creating database table 
+    content = models.TextField()  #creates column that stores text
+    is_public = models.BooleanField(default=True) #true -> public and false private
     topic = models.CharField(max_length=100)
-    file = models.FileField(upload_to='uploads/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='uploads/', null=True, blank=True) #file uploads
+    created_at = models.DateTimeField(auto_now_add=True) #storing time and date of post
