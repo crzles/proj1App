717874100f696012a122d1f8d7d6b1ae2156bf06
LizethLux <lizeth.villanueva01@utrgv.edu>
2026-04-22 18:04:54 +0000

    Post model

--- proj1App/models.py
+++ proj1App/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    is_public = models.BooleanField(default=True)
+    topic = models.CharField(max_length=100)
+    file = models.FileField(upload_to='uploads/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
