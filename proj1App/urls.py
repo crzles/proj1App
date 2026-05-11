2a242e216d2d10efd8612f7e94f7c40c88c9c9f0
LizethLux <lizeth.villanueva01@utrgv.edu>
2026-05-11 03:41:59 -0500

    delete button

--- proj1App/urls.py
+++ proj1App/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name ='home'),
+    path('', views.home, name='home'),
+
+    path('posts/delete/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
